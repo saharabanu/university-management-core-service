e0732d51d2042cb7316476a6dc758ab71341e065
saharabanu <saharabanungn@gmail.com>
2023-08-19 17:34:48 +0600

    first commit

---
+++ src/app/routes/index.ts
@@ -0,0 +1,20 @@
+import express, { Request,Response } from 'express';
+
+const router = express.Router();
+// for example
+const yourMiddlewareFunction = (req:Request, res:Response) => {
+  res.send("Hello from your middleware!");
+};
+
+
+
+const moduleRoutes = [
+  // ... routes
+  {
+    path: "",
+    routes: yourMiddlewareFunction
+  }
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
+export default router;
